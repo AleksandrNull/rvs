8e80b46f445456fcfe4436bcbfb5a686e4c4e8c3
Aleksandr Shaposhnikov <alashai8@gmail.com>
2014-08-26 14:57:32 -0700

    Adding create_db/admin user init functionality

--- manage.py
+++ manage.py
@@ -1,16 +1,21 @@
-from rvs import app,db 
-
-from flask.ext.script import Manager
+from rvs import app,db,models 
+from flask.ext.script import Manager, Command
 from flask.ext.migrate import Migrate, MigrateCommand
+
+
+class Init_App(Command):
+    def run(self):
+        db.create_all()
+        u = models.User(name="admin",passwd="admin")
+        db.session.add(u)
+        db.session.commit()
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128))
+manager.add_command('init', Init_App())
 
 if __name__ == '__main__':
     manager.run()
